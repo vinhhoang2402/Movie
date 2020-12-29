781ca104d4718c41a36772f08d887252193b8896
vinhhoang <vinhthontay113@gmail.com>
2020-12-29 21:13:56 +0700

    rating

---
+++ domain/src/main/java/com/example/domain/usecase/GetMovieRatingUseCase.kt
@@ -0,0 +1,21 @@
+package com.example.domain.usecase
+
+import com.example.domain.common.SingleUseCase
+import com.example.domain.entity.MovieResponseEntity
+import com.example.domain.repository.MovieRepository
+import io.reactivex.Scheduler
+import io.reactivex.Single
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.TimeoutException
+
+class GetMovieRatingUseCase(private val movieRepository: MovieRepository,
+                            uiThread: Scheduler,
+                            executorThread: Scheduler
+    ) : SingleUseCase<MovieResponseEntity>(uiThread, executorThread) {
+
+        override fun create(): Single<MovieResponseEntity> {
+            return movieRepository.getMovieRating().timeout(3, TimeUnit.SECONDS).retry { times, throwable ->
+                times < 5 && throwable is TimeoutException
+            }
+        }
+    }
